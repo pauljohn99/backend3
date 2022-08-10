1a8162dc947b3251887e3a0df7a7a475eb843549
pauljohn99 <paul.j.alapatt123@gmail.com>
2022-08-10 18:05:07 +0530

    m1

--- matrix1.java
+++ matrix1.java
@@ -1,5 +1,32 @@
 package com.orthofx;
-
-public class matrix1 {
+import java.util.Scanner;
+public class Matrix1 {
+	public static void main(String[] args) {
+		Scanner read=new Scanner(System.in);
+		System.out.println("number of rows");
+		int row=read.nextInt();
+		System.out.println("number of columns");
+		int col=read.nextInt();
+		int m1[][]=new int[100][100];
+		int m2[][]=new int[100][100];
+		int sum[][]=new int[100][100];
+		System.out.println("enter elements of matrix 1");
+		for(int i=0;i<row;i++) {
+			for(int j=0;j<col;j++) {
+				m1[i][j]=read.nextInt();}}
+		System.out.println("enter elements of matrix 2");
+		for(int i=0;i<row;i++) {
+			for(int j=0;j<col;j++) {
+				m2[i][j]=read.nextInt();}}
+		for(int i=0;i<row;i++) {
+			for(int j=0;j<col;j++) {
+				sum[i][j]=m1[i][j]+m2[i][j];}}
+		System.out.println("sum matrix");
+		for(int i=0;i<row;i++) {
+			for(int j=0;j<col;j++) {
+				System.out.print(sum[i][j]+ "    ");}
+			System.out.println();}
+		
+		}
 
 }
