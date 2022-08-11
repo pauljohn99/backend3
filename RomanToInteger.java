47a4236117a92debbb1b48bd30fcd25dd1f84d4e
pauljohn99 <paul.j.alapatt123@gmail.com>
2022-08-12 00:16:13 +0530

    showing error

--- RomanToInteger.java
+++ RomanToInteger.java
@@ -6,7 +6,76 @@ public class RomanToInteger {
 		System.out.println("enter roman numeral");
 		String roman=read.nextLine();
 		int n=roman.length();
-		
+		int sum=0,i=0;
+		while(i<n) {
+			if(roman.charAt(i)=='I'){
+				if(roman.charAt(i+1)=='V') {
+					sum=sum+4;
+					i=i+2;
+				}
+				else if(roman.charAt(i+1)=='X') {
+					sum=sum+9;
+					i=i+2;
+				}
+				else {
+					sum=sum+1;
+					i=i+1;
+				}
+			}
+			else if(roman.charAt(i)=='V') {
+				sum=sum+5;
+				i=i+1;
+			}
+			else if(roman.charAt(i)=='X'){
+				if(roman.charAt(i+1)=='L') {
+					sum=sum+40;
+					i=i+2;
+				}
+				else if(roman.charAt(i+1)=='C') {
+					sum=sum+90;
+					i=i+2;
+				}
+				else {
+					sum=sum+10;
+					i=i+1;
+				}
+					}
+			else if(roman.charAt(i)=='L') {
+				sum=sum+50;
+				i=i+1;
+			}
+			else if(roman.charAt(i)=='C'){
+				if(roman.charAt(i+1)=='D') {
+					sum=sum+400;
+					i=i+2;
+				}
+				else if(roman.charAt(i+1)=='M') {
+					sum=sum+900;
+					i=i+2;
+				}
+				else {
+					sum=sum+100;
+					i=i+1;
+				}
+					}
+			else if(roman.charAt(i)=='D') {
+				sum=sum+500;
+				i=i+1;
+			}
+			else if(roman.charAt(i)=='M') {
+				sum=sum+1000;
+				i=i+1;
+			}
+			else
+				System.out.println("invalid roman numeral");
+			
+			
+				
+				
+			}
+		 System.out.println(sum);
+				
+			}
+		}
 		
 
-}
