999e9c1a61febebaa43a23d5f6c92efcf554b707
pauljohn99 <paul.j.alapatt123@gmail.com>
2022-08-11 23:05:35 +0530

    reading romman number

---
+++ RomanToInteger.java
@@ -0,0 +1,12 @@
+package com.orthofx;
+import java.util.Scanner;
+public class RomanToInteger {
+	public static void main(String[] args) {
+		Scanner read=new Scanner(System.in);
+		System.out.println("enter roman numeral");
+		String roman=read.nextLine();
+		int n=roman.length();
+		
+		
+
+}
