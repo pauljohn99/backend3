c6ecccde119e57b9c463732436854567a567e99c
pauljohn99 <paul.j.alapatt123@gmail.com>
2022-08-12 16:14:53 +0530

    code edited completely

--- RomanToInteger.java
+++ RomanToInteger.java
@@ -1,113 +1,49 @@
 package com.orthofx;
 import java.util.Scanner;
 public class RomanToInteger {
+	
+	static int value(char p)
+    {
+   	 switch(p) {
+   		 case 'I':
+   		     return 1;
+   		 case 'V':
+   			 return 5;
+   		 case 'X':
+   			 return 10;
+   		 case 'L':
+   			 return 50;
+   		 case 'C':
+   			 return 100;
+   		 case 'D':
+   			 return 500;
+   		 case 'M':
+   			 return 1000;
+   		default:
+   			return -1;
+   		 }
+    }
+	
 	public static void main(String[] args) {
 		Scanner read=new Scanner(System.in);
 		System.out.println("enter roman numeral");
 		String roman=read.nextLine();
 		int n=roman.length();
-		int sum=0,i=0,k=0;
-		while(i<n) {
-			if(roman.charAt(i)=='I'){
-				if((i+1)==n) {
-					sum=sum+1;
-					i=i+1;
-					}
-				else if(roman.charAt(i+1)=='V') {
-					sum=sum+4;
-					i=i+2;
-				}
-				else if(roman.charAt(i+1)=='X') {
-					sum=sum+9;
-					i=i+2;
-				}
-				else if(roman.charAt(i+1)=='I') {
-					sum=sum+1;
-					i=i+1;
-				}
-				else {
-					k=k+1;
-					i=i+n;
-					System.out.println("invalid numeral");
-				}
-				
-				}
-				
-			else if(roman.charAt(i)=='V') {
-				sum=sum+5;
-				i=i+1;
-			    }
-			else if(roman.charAt(i)=='X'){
-				if((i+1)==n) {
-					sum=sum+10;
-					i=i+1;
-					}
-				else if(roman.charAt(i+1)=='L') {
-					sum=sum+40;
-					i=i+2;
-				}
-				else if(roman.charAt(i+1)=='C') {
-					sum=sum+90;
-					i=i+2;
-				}
-				else if(roman.charAt(i+1)=='X') {
-					sum=sum+10;
-					i=i+1;}
-				else {
-					k=k+1;
-					i=i+n;
-					System.out.println("inavlid numeral");
-				}
-		    }
-			else if(roman.charAt(i)=='L') {
-				sum=sum+50;
-				i=i+1;
-			}
-			else if(roman.charAt(i)=='C'){
-				if((i+1)==n) {
-					sum=sum+100;
-					i=i+1;
-					}
-				if(roman.charAt(i+1)=='D') {
-					sum=sum+400;
-					i=i+2;
-				}
-				else if(roman.charAt(i+1)=='M') {
-					sum=sum+900;
-					i=i+2;
-				}
-				else if(roman.charAt(i+1)=='C') {
-					sum=sum+100;
-					i=i+1;
-				}
-				else {
-					k=k+1;
-					i=i+n;
-					System.out.println("invalid numeral");
-				}
-					}
-			else if(roman.charAt(i)=='D') {
-				sum=sum+500;
-				i=i+1;
-				}
-				
-			else if(roman.charAt(i)=='M') {
-				sum=sum+1000;
-				i=i+1;
+		int a,b,sum=0;
+		for(int i=0;i<n-1;i++) {
+			a=value(roman.charAt(i));
+			b=value(roman.charAt(i+1));
+			if(a>=b) {
+				sum=sum+a;
 			}
 			else {
-				System.out.println("invalid roman numeral");
-			    i=i+n;
-			    k=k+1;
-			    }
-			
-				
-				
-			}
-		if(k==0)
-		 System.out.println(sum);
-				
-			}
+				sum=sum-a;
+			 }
 		}
+		sum=sum+value(roman.charAt(n-1));
+		System.out.println(sum);
+		   }
+	    }  
+
 		
 
